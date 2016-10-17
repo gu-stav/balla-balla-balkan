29ac1b94eaadd7b0483741accbe70e08443b0e2f
Gustav Pursche <pursche@posteo.de>
2016-10-18 01:23:21 +0200

    episode: make soundcloud embed dynamic

--- wordpress/wp-content/themes/balla-balla-balkan/templates/content-single.php
+++ wordpress/wp-content/themes/balla-balla-balkan/templates/content-single.php
@@ -1,15 +1,13 @@
 <?php while (have_posts()) : the_post(); ?>
   <article class="episode">
-    <header class="episode__header">
-      <?php echo the_post_thumbnail('post-thumbnail', array('class' => 'episode__image')); ?>
-
+    <header class="episode__header"
+            style="background-image: url(<?php echo the_post_thumbnail_url('post-thumbnail'); ?>)">
       <div class="episode__canvas">
 
-        <iframe width="100%"
-                height="166"
-                scrolling="no"
-                frameborder="no"
-                src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/283003271&amp;color=ffed00&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;show_artwork=false"></iframe>
+        <?php
+          $soundcloud_embed = get_field('image', get_the_ID());
+          echo $soundcloud_embed;
+        ?>
 
         <h2 class="episode__title">
           <a href="<?php the_permalink(); ?>">
--- wordpress/wp-content/themes/balla-balla-balkan/templates/content.php
+++ wordpress/wp-content/themes/balla-balla-balkan/templates/content.php
@@ -3,11 +3,10 @@
           style="background-image: url(<?php echo the_post_thumbnail_url('post-thumbnail'); ?>)">
     <div class="episode__canvas">
 
-      <iframe width="100%"
-              height="166"
-              scrolling="no"
-              frameborder="no"
-              src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/283003271&amp;color=ffed00&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=false&amp;show_reposts=false&amp;show_artwork=false"></iframe>
+      <?php
+        $soundcloud_embed = get_field('soundcloud-embed', get_the_ID());
+        echo htmlspecialchars_decode($soundcloud_embed);
+      ?>
 
       <h2 class="episode__title">
         <a href="<?php the_permalink(); ?>">
