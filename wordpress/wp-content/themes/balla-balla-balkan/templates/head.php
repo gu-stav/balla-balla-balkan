8bc0f0ab7cff9c2beeb76f2798f64209293cc552
Gustav Pursche <pursche@posteo.de>
2016-10-20 10:07:26 +0200

    wp: enable rss feed

--- wordpress/wp-content/themes/balla-balla-balkan/templates/head.php
+++ wordpress/wp-content/themes/balla-balla-balkan/templates/head.php
@@ -6,6 +6,10 @@
         content="width=device-width, initial-scale=1" />
   <link href="https://fonts.googleapis.com/css?family=Erica+One|Roboto"
         rel="stylesheet" />
+  <link rel="alternate"
+        type="application/rss+xml"
+        title="Ballaballa-Balkan"
+        href="<?php bloginfo('rss2_url'); ?>" />
 
   <?php wp_head(); ?>
 </head>
