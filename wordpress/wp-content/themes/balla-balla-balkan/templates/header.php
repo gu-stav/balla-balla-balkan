2406a9689b4c0c0cb5323e7a4845b43c7f49d303
Gustav Pursche <pursche@posteo.de>
2019-03-07 18:30:20 +0100

    Remove duplicated button

--- wordpress/wp-content/themes/balla-balla-balkan/templates/header.php
+++ wordpress/wp-content/themes/balla-balla-balkan/templates/header.php
@@ -14,7 +14,4 @@
   <p class="header__tagline">
     <?php echo get_bloginfo('description'); ?>
   </p>
-
-  <script type="text/javascript" src="https://steadyhq.com/widget_loader/da5092ad-eb62-40e8-a5a9-b0e2ed4f7391"></script>
-
 </header>
