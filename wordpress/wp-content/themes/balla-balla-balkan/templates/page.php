a5638206038ec858f7d1c14aaf35eb5502c84eb7
Gustav Pursche <pursche@posteo.de>
2020-02-24 19:57:06 +0100

    Simplify page template

--- wordpress/wp-content/themes/balla-balla-balkan/templates/page.php
+++ wordpress/wp-content/themes/balla-balla-balkan/templates/page.php
@@ -1,11 +1,9 @@
-<?php while (have_posts()) : the_post(); ?>
-  <article class="episode">
-     <h2 class="episode__title">
-        <?php the_title(); ?>
-      </h2>
+<article class="episode">
+    <h2 class="episode__title">
+      <?php the_title(); ?>
+    </h2>
 
-    <div class="episode__content">
-      <?php the_content(); ?>
-    </div>
-  </article>
-<?php endwhile; ?>
+  <div class="episode__content">
+    <?php the_content(); ?>
+  </div>
+</article>
