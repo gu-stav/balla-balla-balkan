06df616a0f223937534c6b418e7ac3f3268cc6eb
Gustav Pursche <pursche@posteo.de>
2021-04-17 13:46:28 +0200

    About: fix social media links

--- components/About/About.js
+++ components/About/About.js
@@ -5,29 +5,27 @@ import Stack from "../Stack";
 
 import styles from "./About.module.css";
 
-const About = ({ title, description }) => (
-  <section className={styles.section}>
-    <div className={styles.inner}>
-      <div className={styles.contentContainer}>
-        <Headline level={2}>{title}</Headline>
-        <Paragraph className={styles.description}>{description}</Paragraph>
+const About = ({ title, description, social_media }) => {
+  return (
+    <section className={styles.section}>
+      <div className={styles.inner}>
+        <div className={styles.contentContainer}>
+          <Headline level={2}>{title}</Headline>
+          <Paragraph className={styles.description}>{description}</Paragraph>
 
-        <Stack direction="horizontal" gap="tiny" center>
-          <Paragraph className={styles.followLabel}>
-            Folge dem Podcast auf
-          </Paragraph>
-          <FollowList
-            theme="brand"
-            items={[
-              ["apple", "https://apple.com", "Apple"],
-              ["spotify", "https://spotify.com", "Spotify"],
-              ["soundcloud", "https://soundcloud.com", "Soundcloud"],
-            ]}
-          />
-        </Stack>
+          <Stack direction="horizontal" gap="tiny" center>
+            <Paragraph className={styles.followLabel}>
+              Folge dem Podcast auf
+            </Paragraph>
+            <FollowList
+              theme="brand"
+              items={social_media.map(item => ([item.type, item.url, item.type]))}
+            />
+          </Stack>
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
 
+}
 export default About;
