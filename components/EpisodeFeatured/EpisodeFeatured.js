57c89a01cf7619805e5a79b3ac39fd276e10f6d7
Gustav Pursche <pursche@posteo.de>
2021-05-21 10:15:03 +0200

    EpisodeFeatured: fix blur artifacts

--- components/EpisodeFeatured/EpisodeFeatured.js
+++ components/EpisodeFeatured/EpisodeFeatured.js
@@ -34,12 +34,14 @@ const EpisodeFeatured = ({
     <section className={styles.section}>
       {backgroundImage && (
         <div className={styles.backgroundImageContainer}>
-          <Image
-            src={backgroundImage}
-            objectFit="cover"
-            layout="fill"
-            className={styles.backgroundImage}
-          />
+          <div className={styles.backgroundImageInnerContainer}>
+            <Image
+              src={backgroundImage}
+              objectFit="cover"
+              layout="fill"
+              className={styles.backgroundImage}
+            />
+          </div>
         </div>
       )}
 
