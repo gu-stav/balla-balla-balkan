d1852f127f5b2a7c1f50e4924853ab79f1e40438
Gustav Pursche <pursche@posteo.de>
2021-05-21 11:31:27 +0200

    Next: create redirects from old website

--- next.config.js
+++ next.config.js
@@ -1,7 +1,25 @@
 const withPlugins = require('next-compose-plugins');
 
 module.exports = withPlugins([], {
+  async redirects() {
+    const redirects = require('./redirects.json');
+
+    return redirects.map(([source, destination]) => {
+      if (!source || source.length === 0 || !destination || destination.length === 0) {
+        return null;
+      }
+
+      console.log(`Create redirect: ${source} -> ${destination}`);
+
+      return {
+        source,
+        destination,
+        permanent: true
+      }
+    }).filter(Boolean);
+  },
+
   future: {
-    //webpack5: true
+    webpack5: true
   }
 });
