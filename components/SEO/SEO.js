f3bef2f3ed64256b821003b6a27fbd3a85906126
Gustav Pursche <pursche@posteo.de>
2021-05-23 18:15:18 +0200

    SEO: Optimize page titles

--- components/SEO/SEO.js
+++ components/SEO/SEO.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 export default function SEO({ title = null, description = "Der Podcast für Polemik und Palaver vom Ballaballa-Balkan. Mit dem grimmigen „Kroaten“ Danijel Majić und dem Nationalismusbehinderten Krsto Lazarević." }) {
   return <Head>
     <title>
-      {title ? `${title} |` : ''} Ballaballa-Balkan Podcast
+      {title ? `${title} | Ballaballa-Balkan Podcast` : 'Neues vom Ballaballa-Balkan'}
     </title>
 
     <meta name="description" content={description} />
