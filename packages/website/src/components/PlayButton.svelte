b61ec0bd443783e0912fb19ac7d0c2e8e090214e
Gustav Hansen <gu@stav.dev>
2025-09-18 15:00:21 +0200

    responsive

--- packages/website/src/components/PlayButton.svelte
+++ packages/website/src/components/PlayButton.svelte
@@ -5,7 +5,7 @@
     let { episode } = $props();
 </script>
 
-<button class="bg-black hover:bg-red focus:bg-red cursor-pointer flex font-sans gap-2 items-center py-2 px-4 rounded-sm self-start text-xl text-white" href="/episode/{episode.slug}" on:click|preventDefault={() => setEpisode(episode)}>
+<button class="bg-black hover:bg-red focus:bg-red cursor-pointer sm:flex font-sans gap-2 hidden items-center py-2 px-4 rounded-sm self-start text-xl text-white" href="/episode/{episode.slug}" on:click|preventDefault={() => setEpisode(episode)}>
     Anhören
     <Play class="fill-white h-4 w-4" />
 </button>
